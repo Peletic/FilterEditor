4e43956315ea6a99e2042fca994c4de114e05c11
Peletic <its.peletic@gmail.com>
2024-01-28 01:47:19 -0800

    makes it easier to remove the search contents (changes hover width)

--- src/components/ActionBar/ActionBar.jsx
+++ src/components/ActionBar/ActionBar.jsx
@@ -12,7 +12,7 @@ export default function ActionBar({ handleSearch, suggestions, setFilter, filter
       <input
         type="search"
         id="default-search"
-        className="h-fit block w-1/4 p-4 ml-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white outline-none transition-all ease-in-out hover:w-1/2 duration-200"
+        className="h-fit block w-1/4 p-4 ml-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white outline-none transition-all ease-in-out hover:w-1/3 duration-200"
         placeholder="Search for an item to use in your filter"
         required
         onChange={handleSearch}
